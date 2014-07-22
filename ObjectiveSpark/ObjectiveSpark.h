d460ef466fbf3a3e52c0c844d636810b164b11c7
Evan Coleman <evan@evancoleman.net>
2014-07-22 17:44:57 -0400

    Initial Commit

---
+++ ObjectiveSpark/ObjectiveSpark.h
@@ -0,0 +1,13 @@
+//
+//  ObjectiveSpark.h
+//  ObjectiveSpark
+//
+//  Created by Evan Coleman on 7/22/14.
+//  Copyright (c) 2014 Evan Coleman. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ObjectiveSpark : NSObject
+
+@end
---
+++ ObjectiveSpark/ObjectiveSpark.m
@@ -0,0 +1,13 @@
+//
+//  ObjectiveSpark.m
+//  ObjectiveSpark
+//
+//  Created by Evan Coleman on 7/22/14.
+//  Copyright (c) 2014 Evan Coleman. All rights reserved.
+//
+
+#import "ObjectiveSpark.h"
+
+@implementation ObjectiveSpark
+
+@end
