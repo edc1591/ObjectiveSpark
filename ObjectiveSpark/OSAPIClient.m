8f1a59beb2a7a71dd5f285ad17145f695184364e
Evan Coleman <evan@evancoleman.net>
2014-07-22 18:52:57 -0400

    truncate variable names

--- ObjectiveSpark/OSAPIClient.m
+++ ObjectiveSpark/OSAPIClient.m
@@ -32,6 +32,10 @@
 }
 
 - (void)readVariable:(NSString *)var completion:(void (^)(id, NSError *))completion {
+    // Spark truncates variable names after the 12th character
+    if (var.length > 12) {
+        var = [var substringToIndex:12];
+    }
     [self POST:var parameters:nil success:^(NSURLSessionDataTask *task, id responseObject) {
         if (completion != nil) {
             completion(responseObject, nil);
